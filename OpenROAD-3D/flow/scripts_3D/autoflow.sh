ef8efa7994565f8e908feee5f9770a4d1fbd621b
xiaosinju <syq_nju@foxmail.com>
2025-05-11 17:13:36 +0800

    update support for true-3d placement evaluation

--- OpenROAD-3D/flow/scripts_3D/autoflow.sh
+++ OpenROAD-3D/flow/scripts_3D/autoflow.sh
@@ -8,27 +8,34 @@ export OPENROAD_EXE=$(command -v openroad)
 export YOSYS_EXE=$(command -v yosys)
 if [ "$DESIGN_DIMENSION" = "3D" ]
 then
-    cp ../../Place-3D/install/results/${DEF_VERSION}_3D/${DEF_VERSION}_${DESIGN_DIMENSION}.gp.def designs/nangate45_3D/${DESIGN_NAME}/
+    cp ../../Place-3D/install/results/${DEF_VERSION}_${DESIGN_DIMENSION}/${DEF_VERSION}_${DESIGN_DIMENSION}.gp.def designs/nangate45_3D/${DESIGN_NAME}/
     make DESIGN_CONFIG=designs/nangate45_3D/${DESIGN_NAME}/config_upper_shrink.mk do-autoflow 
     make DESIGN_CONFIG=designs/nangate45_3D/${DESIGN_NAME}/config.mk do-cts_eval 
     make DESIGN_CONFIG=designs/nangate45_3D/${DESIGN_NAME}/config.mk do-hotspot
 
 elif [ "$DESIGN_DIMENSION" = "3D_tiling" ]
 then
-    cp ../../Place-3D/install/results/${DEF_VERSION}_3D_tiling/${DEF_VERSION}_${DESIGN_DIMENSION}.gp.def designs/nangate45_3D/${DESIGN_NAME}/
+    cp ../../Place-3D/install/results/${DEF_VERSION}_${DESIGN_DIMENSION}/${DEF_VERSION}_${DESIGN_DIMENSION}.gp.def designs/nangate45_3D/${DESIGN_NAME}/
     make DESIGN_CONFIG=designs/nangate45_3D/${DESIGN_NAME}/config_tiling_upper_shrink.mk do-autoflow 
     make DESIGN_CONFIG=designs/nangate45_3D/${DESIGN_NAME}/config_tiling.mk do-cts_eval 
     make DESIGN_CONFIG=designs/nangate45_3D/${DESIGN_NAME}/config_tiling.mk do-hotspot 
 
+elif [ "$DESIGN_DIMENSION" = "true_3D" ]
+then
+    cp ../../Place-3D/install/results/${DEF_VERSION}_${DESIGN_DIMENSION}/${DEF_VERSION}_${DESIGN_DIMENSION}.gp.def designs/nangate45_3D/${DESIGN_NAME}/
+    make DESIGN_CONFIG=designs/nangate45_3D/${DESIGN_NAME}/config_true_3D_upper_shrink.mk do-autoflow 
+    make DESIGN_CONFIG=designs/nangate45_3D/${DESIGN_NAME}/config_true_3D.mk do-cts_eval 
+    make DESIGN_CONFIG=designs/nangate45_3D/${DESIGN_NAME}/config_true_3D.mk do-hotspot 
+
 elif [ "$DESIGN_DIMENSION" = "2D" ] 
 then
-    cp ../../Place-3D/install/results/${DEF_VERSION}_2D/${DEF_VERSION}_${DESIGN_DIMENSION}.gp.def designs/nangate45/${DESIGN_NAME}/
+    cp ../../Place-3D/install/results/${DEF_VERSION}_${DESIGN_DIMENSION}/${DEF_VERSION}_${DESIGN_DIMENSION}.gp.def designs/nangate45/${DESIGN_NAME}/
     make DESIGN_CONFIG=designs/nangate45/${DESIGN_NAME}/config_2d_dmp.mk do-def_eval
     make DESIGN_CONFIG=designs/nangate45_3D/${DESIGN_NAME}/config.mk do-hotspot_2D
 
 elif [ "$DESIGN_DIMENSION" = "2D_mp" ]
 then
-    cp ../../Place-3D/install/results/${DEF_VERSION}_2D_mp/${DEF_VERSION}_${DESIGN_DIMENSION}.gp.def designs/nangate45/${DESIGN_NAME}/
+    cp ../../Place-3D/install/results/${DEF_VERSION}_${DESIGN_DIMENSION}/${DEF_VERSION}_${DESIGN_DIMENSION}.gp.def designs/nangate45/${DESIGN_NAME}/
     make DESIGN_CONFIG=designs/nangate45/${DESIGN_NAME}/config_mp.mk do-def_eval
     make DESIGN_CONFIG=designs/nangate45_3D/${DESIGN_NAME}/config.mk do-hotspot_2D
 
