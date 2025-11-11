6d43d8b2f5cab274ae2badbcafcdaefa53610ebd
SWang848 <swang848@outlook.com>
2025-11-11 15:22:35 -0700

    change autoflow script in openroad3d for running hmsa verification

--- OpenROAD-3D/flow/scripts_3D/autoflow.sh
+++ OpenROAD-3D/flow/scripts_3D/autoflow.sh
@@ -3,15 +3,53 @@
 export DESIGN_DIMENSION=$1
 export DEF_VERSION=$2
 export DESIGN_NAME=$3
-echo $DESIGN_DIMENSION, $DEF_VERSION, $DESIGN_NAME
+export HMSA_EVAL_FLAG=$4
+echo $DESIGN_DIMENSION, $DEF_VERSION, $DESIGN_NAME, $HMSA_EVAL_FLAG
 export OPENROAD_EXE=$(command -v openroad)
 export YOSYS_EXE=$(command -v yosys)
 if [ "$DESIGN_DIMENSION" = "3D" ]
 then
-    cp ../../Place-3D/install/results/${DEF_VERSION}_${DESIGN_DIMENSION}/${DEF_VERSION}_${DESIGN_DIMENSION}.gp.def designs/nangate45_3D/${DESIGN_NAME}/
-    make DESIGN_CONFIG=designs/nangate45_3D/${DESIGN_NAME}/config_upper_shrink.mk do-autoflow 
-    make DESIGN_CONFIG=designs/nangate45_3D/${DESIGN_NAME}/config.mk do-cts_eval 
-    make DESIGN_CONFIG=designs/nangate45_3D/${DESIGN_NAME}/config.mk do-hotspot
+    if [ "$HMSA_EVAL_FLAG" = "true" ]
+    then
+        # Iterate through all solution index subfolders
+        for solution_dir in ../../HMSA_solution_eval/${DEF_VERSION}_3D/*/
+        do
+            # Extract solution index from path
+            solution_idx=$(basename "$solution_dir")
+            echo "Processing HMSA solution index: $solution_idx"
+
+            # Copy def file from the solution subfolder
+            cp ${solution_dir}${DEF_VERSION}_3D.gp.def designs/nangate45_3D/${DESIGN_NAME}/
+            
+            # Clean previous build artifacts to ensure fresh build for this solution
+            make DESIGN_CONFIG=designs/nangate45_3D/${DESIGN_NAME}/config_upper_shrink.mk clean_all
+            make DESIGN_CONFIG=designs/nangate45_3D/${DESIGN_NAME}/config.mk clean_all
+            
+            # Run make commands for this solution
+            make DESIGN_CONFIG=designs/nangate45_3D/${DESIGN_NAME}/config_upper_shrink.mk do-autoflow 
+            make DESIGN_CONFIG=designs/nangate45_3D/${DESIGN_NAME}/config.mk do-cts_eval 
+            make DESIGN_CONFIG=designs/nangate45_3D/${DESIGN_NAME}/config.mk do-hotspot
+            
+            # Generate metrics for this specific solution
+            python3 scripts_3D/get_metrics.py
+            
+            # Create directories for storing results
+            mkdir -p ../../HMSA_solution_eval/${DEF_VERSION}_3D/${solution_idx}/openroad_logs/
+            mkdir -p ../../HMSA_solution_eval/${DEF_VERSION}_3D/${solution_idx}/openroad_results/
+            
+            # Copy results to solution folder
+            cp -r ./logs/nangate45_3D/${DEF_VERSION}/3D/* ../../HMSA_solution_eval/${DEF_VERSION}_3D/${solution_idx}/openroad_logs/
+            cp ./final.csv ../../HMSA_solution_eval/${DEF_VERSION}_3D/${solution_idx}/
+            cp -r ./results/nangate45_3D/${DEF_VERSION}/3D/* ../../HMSA_solution_eval/${DEF_VERSION}_3D/${solution_idx}/openroad_results/
+            echo "Completed processing solution index: $solution_idx"
+        done 
+    else
+        cp ../../Place-3D/install/results/${DEF_VERSION}_${DESIGN_DIMENSION}/${DEF_VERSION}_${DESIGN_DIMENSION}.gp.def designs/nangate45_3D/${DESIGN_NAME}/
+        make DESIGN_CONFIG=designs/nangate45_3D/${DESIGN_NAME}/config_upper_shrink.mk do-autoflow 
+        make DESIGN_CONFIG=designs/nangate45_3D/${DESIGN_NAME}/config.mk do-cts_eval 
+        make DESIGN_CONFIG=designs/nangate45_3D/${DESIGN_NAME}/config.mk do-hotspot
+    fi
+    
 
 elif [ "$DESIGN_DIMENSION" = "3D_tiling" ]
 then
